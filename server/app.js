0fc84e50f565bbb23fb55e1fcf8a887e44a4c0b6
supermendel <mendelgabi91@gmail.com>
2023-12-07 20:28:38 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const CodeBlock = require('./Models/Codeblock');
 const {Server} = require('socket.io');
 const app = express();
 const cors = require('cors');
-require('dotenv').config();
+require('.env').config();
 
 
 
