2074759740727a2513316919d19ccc2f4693e416
supermendel <mendelgabi91@gmail.com>
2023-12-07 21:19:13 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -11,11 +11,7 @@ require('dotenv').config();
 
 app.use(cors());
 const uri = process.env.MONGODB_URI;
-mongoose.connect(uri,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+mongoose.connect(uri);
   
 
 
