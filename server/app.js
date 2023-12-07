eca375f7f913fa14c28e3fcfcecb25085126b692
supermendel <mendelgabi91@gmail.com>
2023-12-07 21:49:02 +0200

    Update app.js

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ db.once('open', () => {
 const server = http.createServer(app); 
 const io = new Server(server,{
     cors:{
-        origin: "http://localhost:3000"
+        origin: "https://homeassignmentclient-production.up.railway.app"
     }
 });
 
